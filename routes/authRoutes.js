35baf359241ddf458a1db5980e347c02c187a75e
pandu255200 <pandu09141507@gmail.com>
2025-06-16 14:50:43 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
@@ -7,13 +7,3 @@ router.post('/register', authController.register);
 router.post('/login', authController.login);
 
 module.exports = router;
-=======
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
->>>>>>> 564f2d4532ccc2b38ad214507c143e3ec7a113d6
